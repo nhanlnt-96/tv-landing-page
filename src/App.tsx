f99b751dc5b05acfdaab854a20747d2900ba53f9
Jay <nhanlnt@hotmail.com>
2021-12-02 23:53:09 +0700

    [section01]: Complete create section 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import MainLayout from 'components/mainLayout/MainLayout';
 
-import './styles/App.scss';
+import 'styles/App.scss';
+import 'styles/rwd.scss';
 
 function App() {
-  return <MainLayout />;
+  return (
+    <div className="App">
+      <MainLayout />
+    </div>
+  );
 }
 
 export default App;
--- src/components/mainLayout/MainLayout.tsx
+++ src/components/mainLayout/MainLayout.tsx
@@ -1,13 +1,12 @@
 import React, { FC } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Section01 from 'components/section01/Section01';
+import './MainLayout.scss';
 
 const MainLayout: FC = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Section01 />
-      </Row>
+    <Container className="main-layout" fluid>
+      <Section01 />
     </Container>
   );
 };
