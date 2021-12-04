b4a55cd49e1819af928458ada7ffc9368411b836
Jay <nhanlnt@hotmail.com>
2021-12-04 23:31:09 +0700

    [integrate-api]: Complete create server to post register to google sheet

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const { google } = require('googleapis');
+require('dotenv').config();
+
+const port = process.env.PORT || 3001;
+const app = express();
+const auth = new google.auth.GoogleAuth({
+  keyFile: 'credentials.json',
+  scopes: 'https://www.googleapis.com/auth/spreadsheets'
+});
+let client;
+(async () => {
+  client = await auth.getClient();
+})();
+const googleSheet = google.sheets({ version: 'v4', auth: client });
+const spreadsheetId = process.env.SPREADSHEET_ID;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+  const { fullName, phoneNumber, classes } = req.body;
+  try {
+    const response = await googleSheet.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range: 'Sheet1!A2:D',
+      valueInputOption: 'RAW',
+      resource: {
+        values: [[fullName, phoneNumber, classes, 'Chưa liên hệ']]
+      }
+    });
+    if (response.status === 200) {
+      res.json({ success: true, data: response.config.data.values });
+    } else {
+      res.json({ success: false, response });
+    }
+  } catch (err) {
+    res.json(err);
+  }
+})
+
+app.listen(port, () => {
+  console.log(`server connected to port ${port}`)
+})
