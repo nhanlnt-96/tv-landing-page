e1494b8991182c2aa5c7ab75652be00a37ee2dc6
Thien <nhanlnt@hotmail.com>
2021-12-08 10:30:21 +0700

    [section06]: Initial section 05

---
+++ client/src/components/section06/Section06.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { Container } from 'react-bootstrap';
+
+const Section06: FC = () => {
+  return <Container className="section-06"></Container>;
+};
+
+export default Section06;
--- client/src/pages/homepage/HomePage.tsx
+++ client/src/pages/homepage/HomePage.tsx
@@ -4,6 +4,7 @@ import Section02 from 'section02/Section02';
 import Section03 from 'section03/Section03';
 import Section04 from 'section04/Section04';
 import Section05 from 'section05/Section05';
+import Section06 from 'section06/Section06';
 import { Container } from 'react-bootstrap';
 import './HomePage.scss';
 
@@ -28,6 +29,9 @@ const HomePage: FC = () => {
       </div>
       <div className="space-top">
         <Section05 />
+      </div>
+      <div className="space-top">
+        <Section06 />
       </div>
     </Container>
   );
