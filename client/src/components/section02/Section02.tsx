0394831c67cf85f0bf665e7645be45341fc22c3b
nhanlnt-96 <nhanlnt@hotmail.com>
2021-12-17 15:14:47 +0700

    [fixbug/section02]: Complete fixbug section 02

--- client/src/components/section02/Section02.tsx
+++ client/src/components/section02/Section02.tsx
@@ -9,23 +9,9 @@ const Section02: FC<IProps> = ({ click }) => {
   return (
     <Container className="section-02">
       <Row className="section02-title">
-        <p>NHỮNG LO LẮNG CỦA CHA MẸ KHI CHO TRẺ HỌC TIẾNG ANH SỚM</p>
+        <p>8 LÝ DO ĐỂ QUÝ PHỤ HUYNH LỰA CHỌN TAN VAN E-LEARNING CHO CON</p>
       </Row>
       <Diagram />
-      <Row className="section02-description">
-        <p>
-          <b>
-            Một ngôn ngữ chỉ cho con một con đường nhưng hai ngôn ngữ sẽ mở ra
-            cho con nhiều ngã rẽ mới mà con được quyền chọn lựa.
-          </b>
-          Giai đoạn 4-7 tuổi là độ tuổi vàng mà con có thể tiếp thu, học hỏi và
-          bứt phá tiếng Anh một cách đáng kinh ngạc. Và bí quyết của 99% thần
-          đồng tiếng Anh đều nằm ở phương pháp học phù hợp và môi trường chuẩn
-          để trẻ có được nền tảng vững chắc nhất. Hãy để Edupia Kid xóa bỏ những
-          lo lắng trên của ba mẹ và giúp con phát triển vượt bậc trong giai đoạn
-          vàng này nhờ những phương pháp đặc biệt!
-        </p>
-      </Row>
       <Row className="section02-register-btn">
         <button onClick={click}>NHẬN TƯ VẤN</button>
       </Row>
