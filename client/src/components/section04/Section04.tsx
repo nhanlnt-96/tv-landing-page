3e01979b2398675624ab2cde1117a81ca69763f4
nhanlnt-96 <nhanlnt@hotmail.com>
2021-12-17 17:00:37 +0700

    [fixbug/section04]: Complete fixbug section 04

--- client/src/components/section04/Section04.tsx
+++ client/src/components/section04/Section04.tsx
@@ -1,30 +1,46 @@
-import React, { FC } from 'react';
-import { Row } from 'react-bootstrap';
-import StarIcon from '@mui/icons-material/Star';
+import React, { FC, useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
 import { section04Content } from 'configs/section04Content';
-import Chicken from 'imgs/chicken.png';
 import { IProps } from 'model/props';
+import Test1 from 'imgs/bg.jpg';
+
 import './Section04.scss';
 
 const Section04: FC<IProps> = ({ click }) => {
+  const [imgSelect, setImgSelect] = useState<any>(Test1);
+  const [isSelect, setIsSelect] = useState<number>(0);
+  const onViewImgDivClick = (img: any, index: number) => {
+    setIsSelect(index);
+    setImgSelect(img);
+  };
   return (
     <Row className="section-04">
       <div className="section04-container">
         <div className="section04-content">
-          <div className="content-title">
-            <h6>KHÓA HỌC TRÊN EDUPIA KID MANG LẠI ĐIỀU GÌ CHO CON?</h6>
-          </div>
-          <div className="content-items">
-            {section04Content.map((val, index) => (
-              <p key={index}>
-                <StarIcon />
-                {val.content}
-              </p>
-            ))}
-          </div>
-        </div>
-        <div className="section04-img">
-          <img src={Chicken} alt="tv-landing-page" />
+          <Row className="content-title">
+            <h6>
+              KHÓA HỌC LÀM QUEN TIẾNG ANH MẦM NON TAN VAN E-LEARNING MANG LẠI
+              ĐIỀU GÌ CHO CON?
+            </h6>
+          </Row>
+          <Row className="content-items">
+            <Col lg={7} md={6} xs={12} className="content">
+              {section04Content.map((val, index) => (
+                <div
+                  className={`item ${
+                    index === isSelect && 'content-item-active'
+                  }`}
+                  onClick={() => onViewImgDivClick(val.img, index)}
+                >
+                  <p>{val.title}</p>
+                  <span>{val.content}</span>
+                </div>
+              ))}
+            </Col>
+            <Col lg={5} md={6} xs={12} className="img">
+              <img src={imgSelect} alt="shdfhkdhk" />
+            </Col>
+          </Row>
         </div>
         <div className="section04-btn">
           <button onClick={click}>NHẬN TƯ VẤN</button>
