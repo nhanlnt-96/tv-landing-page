dde4599da1f243d7317c5d7caf18fd99c1309cec
nhanlnt-96 <nhanlnt@hotmail.com>
2021-12-21 21:54:04 +0700

    [section12]: Initial section 12

---
+++ client/src/components/section12/Section12.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import {Container} from "react-bootstrap";
+
+const Section12: FC = () => {
+  return (
+      <Container className="section-12">
+        <Title
+      </Container>
+  );
+};
+
+export default Section12;
