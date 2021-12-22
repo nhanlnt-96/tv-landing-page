de600800cd02fb983de68bd9166a1ffc9b856610
nhanlnt-96 <nhanlnt@hotmail.com>
2021-12-22 15:40:52 +0700

    [section01]: Complete fixbug section01

--- client/src/components/section01/Section01.tsx
+++ client/src/components/section01/Section01.tsx
@@ -5,6 +5,7 @@ import Header from 'header/Header';
 import { IProps } from 'model/props';
 
 import './Section01.scss';
+import AnimateText from 'animateText/AnimateText';
 
 const Section01: FC<IProps> = ({ click }) => {
   return (
@@ -33,15 +34,8 @@ const Section01: FC<IProps> = ({ click }) => {
                 lg={6}
                 className="register-desc col-height d-flex flex-column justify-content-center align-items-center"
               >
-                <h2 className="register-title">ĐĂNG KÝ NGAY</h2>
-                {/*<p className="register-subtitle">*/}
-                {/*  Chương trình ưu đãi dành riêng cho bé học tại{' '}*/}
-                {/*  <strong style={{ color: '#ff0000' }}>*/}
-                {/*    308 trường Mầm non*/}
-                {/*  </strong>{' '}*/}
-                {/*  liên kết với*/}
-                {/*</p>*/}
-                {/*<strong>Trung Tâm Ngoại Ngữ Tân Văn</strong>*/}
+                {/*<h2 className="register-title">ĐĂNG KÝ NGAY</h2>*/}
+                <AnimateText text={'ĐĂNG KÝ NGAY'} />
               </Col>
               <Col sm={12} md={6} lg={6} className="border-left-line">
                 <RegisterForm />
--- client/src/components/section12/components/MessageBox.tsx
+++ client/src/components/section12/components/MessageBox.tsx
@@ -1,17 +1,17 @@
 import { FC } from 'react';
-import Uri from 'imgs/uri.png';
 
 interface Props {
   title: string;
   price: string;
+  avatar: any;
 }
 
-export const MessageBox: FC<Props> = ({ title, price }) => {
+export const MessageBox: FC<Props> = ({ title, price, avatar }) => {
   return (
     <div className="message-body">
       <div className="message-avatar">
         <div className="avatar">
-          <img src={Uri} alt="tv-e-learning" />
+          <img src={avatar} alt="tv-e-learning" />
         </div>
       </div>
       <div className="message-content">
