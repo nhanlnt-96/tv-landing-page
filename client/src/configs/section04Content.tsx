ef7eac4e3aacac50dc479b34ce1e8d55ec155640
nhanlnt-96 <nhanlnt@hotmail.com>
2021-12-29 16:21:43 +0700

    [master]: Update img for section04

--- client/src/configs/section04Content.tsx
+++ client/src/configs/section04Content.tsx
@@ -1,7 +1,7 @@
-import Test1 from 'imgs/bg.jpg';
-import Test2 from 'imgs/pronunciation.jpeg';
-import Test3 from 'imgs/bg2.jpeg';
-import Test4 from 'imgs/bg3.png';
+import Img1 from 'imgs/learning1.gif';
+import Img2 from 'imgs/pronunciation.jpeg';
+import Img3 from 'imgs/learning3.gif';
+import Img4 from 'imgs/learning4.gif';
 
 interface ISection04Content {
   title: string;
@@ -15,27 +15,27 @@ export const section04Content: ISection04Content[] = [
     content:
       'Con hứng thú học tập với phương pháp giảng dạy “Lấy trẻ làm trung tâm” và bài giảng được tích hợp game vui' +
       ' nhộn.',
-    img: Test1,
+    img: Img1,
   },
   {
     title: 'Phát âm',
     content:
       'Con học phát âm với giáo viên bản ngữ chuẩn, giúp con tự tin\n' +
       '                  giao tiếp.',
-    img: Test2,
+    img: Img2,
   },
   {
     title: 'Ghi nhớ',
     content:
       'Con ghi nhớ từ vựng nhanh nhờ phương pháp học qua hình ảnh\n' +
       '                  minh họa thực tế và vui nhộn.',
-    img: Test3,
+    img: Img3,
   },
   {
     title: 'Hỗ trợ',
     content:
       'Đội ngũ chuyên viên giáo vụ và giáo viên luôn sẵn sàng hỗ trợ\n' +
       '                  con học tập.',
-    img: Test4,
+    img: Img4,
   },
 ];
