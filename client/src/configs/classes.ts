245391cb1faef60d24feec74fe030a32f7a4260d
nhanlnt-96 <nhanlnt@hotmail.com>
2022-01-01 22:47:43 +0700

    [master]: update classes data

--- client/src/configs/classes.ts
+++ client/src/configs/classes.ts
@@ -5,10 +5,6 @@ export interface IClasses {
 
 export const classes: IClasses[] = [
   {
-    value: 1,
-    label: 'Lớp Mẫu giáo',
-  },
-  {
     value: 2,
     label: 'Lớp Mầm',
   },
@@ -19,26 +15,6 @@ export const classes: IClasses[] = [
   {
     value: 4,
     label: 'Lớp Lá',
-  },
-  {
-    value: 5,
-    label: 'Lớp 1',
-  },
-  {
-    value: 6,
-    label: 'Lớp 2',
-  },
-  {
-    value: 7,
-    label: 'Lớp 3',
-  },
-  {
-    value: 8,
-    label: 'Lớp 4',
-  },
-  {
-    value: 9,
-    label: 'Lớp 5',
   },
   {
     value: 10,
